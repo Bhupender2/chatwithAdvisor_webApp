fa63184d59652fde33192cd29e7989425487f607
Bhupender2 <b.mehtaa2021@gmail.com>
2026-05-21 16:11:02 +0530

    done here the auth service

--- services/auth-service.ts
+++ services/auth-service.ts
@@ -1,1 +1,7 @@
-export const loginUser = async () => {};
+import { API_GROUPS } from "@/lib/api-endpoints";
+import axios from "axios";
+
+export const loginUser = async () => {
+  const response = await axios.post(API_GROUPS.staffLogin, {});
+  return response.data; // returning response here
+};
