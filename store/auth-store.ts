7702d2ebcc62df6e6f4affa7c40d98540fe78961
Bhupender2 <b.mehtaa2021@gmail.com>
2026-05-22 15:37:21 +0530

    auth store has been added

---
+++ store/auth-store.ts
@@ -0,0 +1,24 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+
+interface AuthState {
+  token: string | null;
+  senderId: string | null;
+  name: string | null;
+  role: string | null;
+  appAccess?: string | null;
+  notificationToken?: string | null;
+  pkg?: string | null;
+  setAuth: (
+    token?: string,
+    senderId?: string,
+    name?: string,
+    role?: string,
+    appAccess?: string,
+    pkg?: string,
+  ) => void;
+  setNotificationToken: (notificationToken: string) => void;
+  clearAuth: () => void;
+}
+
+export const AuthStore = create<AuthState>()(persist());
