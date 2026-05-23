5aba43f4b97c36f45b9e60bfd491e8ffe4d05234
Bhupender2 <b.mehtaa2021@gmail.com>
2026-05-23 13:27:18 +0530

    api-endpoints added here

--- lib/api-endpoints.ts
+++ lib/api-endpoints.ts
@@ -1,6 +1,7 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE; //"https://lms.neetadvisor.in/api"
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE; //"https://lms.neetadvisor.in/api/v1"
 
 export const API_GROUPS = {
   staffLogin: `${BASE_URL}/staffLogin`,
   getDefaultGroup: `${BASE_URL}/conversation/getAlertConversations`,
+  getPreviousChat: `${BASE_URL}/chat/getPreviousChat`,
 };
