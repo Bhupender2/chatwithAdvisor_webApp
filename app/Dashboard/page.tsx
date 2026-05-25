9c8cc04c4a3e077bb3ab3b9e6a38df7ad2f18dbf
Bhupender2 <b.mehtaa2021@gmail.com>
2026-05-25 15:14:53 +0530

    page.tsx socket has been implemented

--- app/Dashboard/page.tsx
+++ app/Dashboard/page.tsx
@@ -4,8 +4,29 @@ import { AppHeader } from "@/components/dashboard/app-header";
 import ChatArea from "@/components/dashboard/chat-area";
 import DashboardSidebar from "@/components/dashboard/sidebar";
 import { SidebarProvider } from "@/components/ui/sidebar";
+import { disconnectSocket, getSocket } from "@/services/socket-service";
+import { useAuthStore } from "@/store/auth-store";
+import { useEffect } from "react";
 
 export default function Dashboard() {
+  const token = useAuthStore((state) => state.token);
+  useEffect(() => {
+    if (!token) return;
+
+    const socket = getSocket(token);
+
+    socket.on("connect", () => {
+      console.log("Socket connected", socket.id);
+    });
+
+    socket.on("connect_error", (error) => {
+      console.log("Socket error:", error.message);
+    });
+
+    return () => {
+      disconnectSocket();
+    };
+  }, [token]);
   return (
     <SidebarProvider>
       <div className="flex flex-col h-screen w-full">
