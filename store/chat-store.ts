ccdfccea73449e4d19e83bbd051fb6259c92994c
Bhupender2 <b.mehtaa2021@gmail.com>
2026-05-25 12:59:32 +0530

    chat-store done

--- store/chat-store.ts
+++ store/chat-store.ts
@@ -1,8 +1,24 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
 
+interface Message {
+  _id: string;
+  senderId?: {
+    _id: string;
+    name: string;
+    profile: string;
+  };
+  conversationId: string;
+  content: string;
+  type: "text" | "image" | "video" | "audio" | "pdf";
+  text?: string; // like when i am getting text it doesnt have the text key others have
+  createdAt: string;
+  status: "sending" | "sent" | "failed";
+}
+
 interface ConversationState {
   conversationId: string | null;
+  liveMessages: Message[];
   setConversationId: (conversationId: string) => void;
   clearChat: () => void;
 }
@@ -11,9 +27,11 @@ export const useChatStore = create<ConversationState>()(
   persist(
     (set) => ({
       conversationId: null,
+      liveMessages: [], // socket + optimistic messages yahan
       setConversationId: (conversationId) =>
         set({
           conversationId: conversationId,
+          // liveMessages;[] //  naya conversationId open hote hi clear kardo
         }),
       clearChat: () => {
         set({
