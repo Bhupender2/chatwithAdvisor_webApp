dc56ee2633ee26634a8c4c7461f4f2fbe4176ace
Bhupender2 <b.mehtaa2021@gmail.com>
2026-08-05 15:32:17 +0530

    uselogout mutation hook has been added

---
+++ hooks/mutations/use-logout.ts
@@ -0,0 +1,8 @@
+import { logoutUser } from "@/services/auth-service";
+import { useMutation } from "@tanstack/react-query";
+
+export const useLogout = () => {
+  return useMutation({
+    mutationFn: logoutUser,
+  });
+};
