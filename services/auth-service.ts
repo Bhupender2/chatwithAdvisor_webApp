c89028f1f3d35facdde60ffba1bc37dc577aa0cf
Bhupender2 <b.mehtaa2021@gmail.com>
2026-08-05 15:13:29 +0530

    logout api is been added here

--- services/auth-service.ts
+++ services/auth-service.ts
@@ -1,8 +1,23 @@
 import { API_GROUPS } from "@/lib/api-endpoints";
+import { useAuthStore } from "@/store/auth-store";
 import { EmployeeData } from "@/types/auth";
 import axios from "axios";
 
 export const loginUser = async (employeeData: EmployeeData) => {
   const response = await axios.post(API_GROUPS.staffLogin, employeeData);
   return response.data; // returning response here
+};
+
+export const logoutUser = async () => {
+  const accessToken = useAuthStore().accessToken;
+  const response = await axios.post(
+    API_GROUPS.logout,
+    {},
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    },
+  );
+  return response.data;
 };
