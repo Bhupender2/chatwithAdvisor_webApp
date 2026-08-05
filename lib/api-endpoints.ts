2a555abd510e5430288c60ba44cb860676c200dc
Bhupender2 <b.mehtaa2021@gmail.com>
2026-08-05 14:24:27 +0530

    logout endpoints has been added here

--- lib/api-endpoints.ts
+++ lib/api-endpoints.ts
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE; //"https://lms.neetadvisor.in/api/v1"
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE; //"https://chat.neetadvisor.com/api/v1"
 
 export const API_GROUPS = {
   staffLogin: `${BASE_URL}/staffLogin`,
