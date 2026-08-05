ffd455cfdd09fe1c03342586f7f6ff7115934c15
Bhupender2 <b.mehtaa2021@gmail.com>
2026-08-05 14:22:52 +0530

    api-endpoints has been added

--- lib/api-endpoints.ts
+++ lib/api-endpoints.ts
@@ -5,4 +5,6 @@ export const API_GROUPS = {
   getDefaultGroup: `${BASE_URL}/conversation/getAlertConversations`,
   getPreviousChat: `${BASE_URL}/chat/getPreviousChat`,
   upload: `${BASE_URL}/upload`,
+  logout: `${BASE_URL}/staffLogout
+`,
 };
